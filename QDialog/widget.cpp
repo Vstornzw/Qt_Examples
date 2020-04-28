465c3bf4b23d484e266040fe321348fae9892159
weizhao <Vstornzw@163.com>
2020-04-28 21:25:28 +0800

    add Dialog

---
+++ QDialog/widget.cpp
@@ -0,0 +1,30 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+
+Widget::Widget(QWidget *parent) :
+  QWidget(parent),
+  ui(new Ui::Widget),
+  status(false)
+{
+  ui->setupUi(this);
+  dialog = new Dialog(this);
+  dialog->setModal(true);
+
+  connect(ui->push_button_,SIGNAL(clicked(bool)),this,SLOT(onShowDialog()));
+}
+
+Widget::~Widget()
+{
+  delete ui;
+}
+
+void Widget::onShowDialog() {
+  if(!status){
+    dialog->show();
+    status = true;
+  } else {
+    dialog->hide();
+    status = false;
+  }
+}
---
+++ QDialog/widget.h
@@ -0,0 +1,27 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include "dialog.h"
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+  Q_OBJECT
+
+public slots:
+  void onShowDialog();
+public:
+  explicit Widget(QWidget *parent = 0);
+  ~Widget();
+
+private:
+  Ui::Widget *ui;
+  Dialog *dialog;
+  bool status;
+};
+
+#endif // WIDGET_H
